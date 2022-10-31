2b705d17afd7c56d52c0fe76591e781690acf076
SirGay <serega112015@gmail.com>
2022-10-31 14:48:50 +0300

    I did exercise 2 and commit it)

---
+++ src/Homework2/Exercise1/Exercise1.java
@@ -0,0 +1,26 @@
+package Homework2.Exercise1;
+
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+
+        int first, second;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите первый разряд двухмерного массива: ");
+        first = sc.nextInt();
+
+        System.out.print("Введите второй разряд двухмерного массива: ");
+        second = sc.nextInt();
+
+        int[][] Array = new int[first][second];
+
+
+    }
+}
+
+
---
+++ src/Homework2/Exercise2/Exercise2.java
@@ -0,0 +1,31 @@
+package Homework2.Exercise2;
+
+
+import java.util.Scanner;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+
+        int first, second, i, j;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите первый разряд двухмерного массива: "); //Устанавливаем кол-во строк в двумерном массиве
+        first = sc.nextInt();
+
+        System.out.print("Введите второй разряд двухмерного массива: "); //Устанавливаем кол-во элементов в каждой строке
+        second = sc.nextInt();
+
+        int[][] Array = new int[first][second];
+
+        for (i = 0; i < first; i++) {
+            for (j = 0; j < second; j++) {
+                Array[i][j] = (int) (Math.random() * 100); // Каждое эдемент - случайное целое число от 0 до 100
+                System.out.print(Array[i][j] + " ");       // Выводим на консоль получившийся массив
+            }
+            System.out.println();
+        }
+    }
+}
+
+
